b2b8909a47e242e9c43058d859890ff77929ad65
d1m1d1n <dmitriy-loginov-02@mail.ru>
2025-10-01 15:38:01 +0300

    3_lab: constructor, first method - adding

--- main.cpp
+++ main.cpp
@@ -2,6 +2,24 @@
 
 // калькулятор
 
+class Calculator {
+private:
+    double result; // результат вычислений
+
+public:
+    Calculator():result(0.0){} // конструктор
+    Calculator (double starter) : result(starter) {} // конструктор с начальным значением
+
+    // методы - основные операции
+
+    // 1. сложение
+    void summ(double value){
+        result += value;
+    }
+
+
+};
+
 using namespace std;
 
 
